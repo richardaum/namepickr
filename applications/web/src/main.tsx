88681ae2ae293ec957229f0dc68745a6530c1b2c
Richard <richard.lopes92@gmail.com>
2021-09-11 14:14:53 -0300

    chore: add pages and utils folders

--- applications/web/src/main.tsx
+++ applications/web/src/main.tsx
@@ -1,20 +1,17 @@
 import "./index.css";
 
-import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-import { nest } from "./nest";
+import App from "./pages/App";
 import { AppThemeProvider } from "./theme/AppThemeProvider";
+import { nest } from "./utils/nest";
 
 const Providers = nest(React.StrictMode, AppThemeProvider);
 
 ReactDOM.render(
-  <>
-    <Providers>
-      <App />
-    </Providers>
-  </>,
+  <Providers>
+    <App />
+  </Providers>,
   document.getElementById("root")
 );
